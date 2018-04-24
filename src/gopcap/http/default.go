3b221096bc78411ab1cb5985f20577187cb7ebae
Lqlsoftware <lqlsoft@hotmail.com>
2018-04-24 16:37:16 +0800

    add gzip compress

--- src/gopcap/http/default.go
+++ src/gopcap/http/default.go
@@ -1,6 +1,11 @@
 package http
 
-import "io/ioutil"
+import (
+	"bytes"
+	"compress/gzip"
+	"io/ioutil"
+	"strings"
+)
 
 // Default GET method
 // 		root/URL
@@ -11,6 +16,29 @@ func DefaultGETHandler(request *HttpRequest, response *HttpResponse) {
 		response.contents = []byte("<html>ERROR 404!</html>")
 		return
 	}
+
+	// gzip 压缩
+	if encoding,ok := (*request.header)["Accept-Encoding"];ok {
+		encodes := strings.Split(encoding, ", ")
+		// 检查浏览器是否支持gzip压缩
+		for _,v := range encodes {
+			// 支持gzip压缩
+			if v == "gzip" {
+				// 压缩数据
+				var b bytes.Buffer
+				w := gzip.NewWriter(&b)
+				w.Write(dat)
+				w.Flush()
+				dat = b.Bytes()
+
+				// 设置返回header 通知浏览器压缩格式
+				(*response.header)["Content-Encoding"] = "gzip"
+				break
+			}
+		}
+	}
+
+	// 设置Content-Type
 	(*response.header)["Content-Type"] = getContentType(*request.url) + "; charset=utf-8"
 	response.stateCode = OK
 	response.contents = dat
